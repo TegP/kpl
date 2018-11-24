9165dd715406e11280b96dcf53cde565cc1de2c7
admin604 <admin604@DESKTOP-JCGQC1U>
2018-11-24 08:07:48 +0700

    test tambah class Tim

---
+++ src/main/java/org/ifunpas/kpl/ContohVersioning/Tim.java
@@ -0,0 +1,14 @@
+package org.ifunpas.kpl.ContohVersioning;
+
+public class Tim {
+	private String nama;
+	private List<Pemain> pemainInti = new ArrayList<Pemain>();
+	
+	public Tim() {
+		nama = "";
+	}
+	
+	public Tim(String nama) {
+		this.nama = nama;
+	}
+}
