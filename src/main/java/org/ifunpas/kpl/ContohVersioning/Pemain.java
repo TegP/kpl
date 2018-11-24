7b7f9bc093b0ca7403b1599a798f68c061dd3808
admin604 <admin604@DESKTOP-VFGR3B2>
2018-11-24 08:04:32 +0700

    Second Commit

---
+++ src/main/java/org/ifunpas/kpl/ContohVersioning/Pemain.java
@@ -0,0 +1,12 @@
+package org.ifunpas.kpl.ContohVersioning;
+
+public class Pemain {
+	private String nama;
+	private int noPunggung;
+	private int tes;
+	
+	public Pemain (String nama, int noPunggung) {
+		this.nama = nama;
+		this.noPunggung = noPunggung;
+	}
+}
